d82f340504c4c482c81dec91f7285daf2dbdf5f8
Guilherme Félix <guilher.felixrosa@gmail.com>
2022-06-04 10:03:14 -0300

    Creating scripts classes

--- src/classes/Cliente.java
+++ src/classes/Cliente.java
@@ -1,13 +1,81 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package classes;
 
-/**
- *
- * @author IFTM
- */
 public class Cliente {
+    //Variáveis
+    private String idCliente;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    //Construtor padrão
+    public Cliente() {
+        this.idCliente = "Sem id";
+        this.nome = "Sem nome";
+        this.email = "Sem e-mail";
+        this.telefone = "Sem telefone";
+        this.endereco = "Sem endereço";
+    }
+    
+    //COnstrutor de inicialização
+    public Cliente(String idCliente, String nome, String email, String telefone, String endereco) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    //Getters e setters
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    //toString
+    @Override
+    public String toString() {
+        return "\n Cliente: " 
+                + "\n Id cliente: " + idCliente 
+                + "\n Nome: " + nome 
+                + "\n E-mail: " + email 
+                + "\n Telefone: " + telefone 
+                + "\n Endereço: " + endereco;
+    }
     
 }
--- src/classes/Produto.java
+++ src/classes/Produto.java
@@ -1,13 +1,107 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package classes;
 
-/**
- *
- * @author IFTM
- */
+import java.time.LocalDate;
+
 public class Produto {
+    //Variáveis
+    private String idProduto;
+    private float valor;
+    private String descricao;
+    private String tipoDaUva;
+    private int anoDaSafra;
+    private int quantidade;
+    private LocalDate diaDaVenda;
+    
+    //Contrutor padrão
+    public Produto() {
+        this.idProduto = "Sem id";
+        this.valor = 0;
+        this.descricao = "Sem descrição";
+        this.tipoDaUva = "Sem tipo de uva";
+        this.anoDaSafra = 0;
+        this.quantidade = 0;
+        this.diaDaVenda = LocalDate.now();
+    }
+    
+    //Construtor de inicialização
+    public Produto(String idProduto, float valor, String descricao, String tipoDaUva, int anoDaSafra, int quantidade, LocalDate diaDaVenda) {
+        this.idProduto = idProduto;
+        this.valor = valor;
+        this.descricao = descricao;
+        this.tipoDaUva = tipoDaUva;
+        this.anoDaSafra = anoDaSafra;
+        this.quantidade = quantidade;
+        this.diaDaVenda = diaDaVenda;
+    }
+    
+    //Getter e setters
+    public String getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(String idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipoDaUva() {
+        return tipoDaUva;
+    }
+
+    public void setTipoDaUva(String tipoDaUva) {
+        this.tipoDaUva = tipoDaUva;
+    }
+
+    public int getAnoDaSafra() {
+        return anoDaSafra;
+    }
+
+    public void setAnoDaSafra(int anoDaSafra) {
+        this.anoDaSafra = anoDaSafra;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getDiaDaVenda() {
+        return diaDaVenda;
+    }
+
+    public void setDiaDaVenda(LocalDate diaDaVenda) {
+        this.diaDaVenda = diaDaVenda;
+    }
+    
+    //toString
+    @Override
+    public String toString() {
+        return "\n Produto: " 
+                + "\n Id produto: " + idProduto 
+                + "\n Valor: " + valor 
+                + "\n Descrição: " + descricao 
+                + "\n Tipo da uva: " + tipoDaUva 
+                + "\n Ano da safra: " + anoDaSafra 
+                + "\n Quantidade: " + quantidade 
+                + "\n Dia da venda: " + diaDaVenda;
+    }
     
 }
